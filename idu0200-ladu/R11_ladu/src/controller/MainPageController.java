d44d0aa8e0798dd779627f5853c9ec64880c0042
kairiryytel@gmail.com <kairiryytel@gmail.com@2118312b-7a65-5f19-f1be-7e2d642fecc0>
2013-05-09 14:16:48 +0000

    Parandasin warninguid.

--- idu0200-ladu/R11_ladu/src/controller/MainPageController.java
+++ idu0200-ladu/R11_ladu/src/controller/MainPageController.java
@@ -2,19 +2,10 @@ package controller;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.List;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.hibernate.Session;
-
-import model.ItemType;
-
-import util.EventManager;
-import util.HibernateUtil;
 import view.DrawCatalog;
 
 public class MainPageController extends BaseController {
--- idu0200-ladu/R11_ladu/src/util/HibernateUtil.java
+++ idu0200-ladu/R11_ladu/src/util/HibernateUtil.java
@@ -7,6 +7,7 @@ public class HibernateUtil {
 
     private static final SessionFactory sessionFactory = buildSessionFactory();
 
+	@SuppressWarnings("deprecation")
 	public static SessionFactory buildSessionFactory() {
         try {
             // Create the SessionFactory from hibernate.cfg.xml
